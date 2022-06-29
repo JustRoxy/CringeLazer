878a1d6885214165434c481ccecba0c1a7c66bea
JustRoxy <justroxyosu@inbox.ru>
2022-06-30 01:16:51 +0300

    Add last update cache

--- CringeLazer.Bancho/Middleware/LastVisitMiddleware.cs
+++ CringeLazer.Bancho/Middleware/LastVisitMiddleware.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using CringeLazer.Bancho.Domain;
 using MongoDB.Entities;
 
@@ -5,6 +6,7 @@ namespace CringeLazer.Bancho.Middleware;
 
 public class LastVisitMiddleware : IMiddleware
 {
+    private static readonly ConcurrentDictionary<ulong, DateTime> _updateCache = new();
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var claim = context.User.Claims.FirstOrDefault(x => x.Type == "Id");
@@ -16,9 +18,16 @@ public class LastVisitMiddleware : IMiddleware
 
         await next(context);
 
-        var name = ulong.Parse(claim.Value);
+        var id = ulong.Parse(claim.Value);
+        var now = DateTime.UtcNow;
+        var updated = _updateCache.GetOrAdd(id, now);
+        if (updated.AddMinutes(5) > now)
+        {
+            return;
+        }
+
         _ = DB.Update<User>() //Fire-and-forget update
-            .Match(x => x.Id == name)
+            .Match(x => x.Id == id)
             .Modify(x => x.Set(v => v.LastVisit, DateTime.UtcNow))
             .ExecuteAsync();
     }
